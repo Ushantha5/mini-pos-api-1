626348b9210b0f3fcd08b87cd02ffa05f75b271f
ushantha <ushanthamr@gmail.com>
2025-10-17 04:46:33 +0530

    db connectet

---
+++ config/db.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+dotenv.config();
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log("MongoDB Coecting...");
+  } catch (error) {
+    console.error(error.massage);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
